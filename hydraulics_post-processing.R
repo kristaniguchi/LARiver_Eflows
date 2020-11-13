48e90272402c39b0f7ebef78f216d72ce174d02e
kristaniguchi <kristinetq@sccwrp.org>
2020-11-13 11:18:31 -0800

    hydraulic filter if only MC

--- hydraulics_post-processing.R
+++ hydraulics_post-processing.R
@@ -7,15 +7,21 @@
 
 
 #Hydraulics raw output directory
-raw.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/hydraulics-results-v3-201007/"
+#raw.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/hydraulics-results-v3-201007/"
+#scenarios hydraulics:
+raw.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/Scnearios_hydraulics_10132020/results-hydraulics/"
+
 files <- list.files(raw.dir, full.names = TRUE)
 files.short <- list.files(raw.dir)
 
 #post-processed directory
-out.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/post_processed_hydraulics_201008/"
+#out.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/post_processed_hydraulics_201008/"
+#scenario output dir
+out.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/Scnearios_hydraulics_10132020/results-hydraulics_postprocessed/"
 
 #loop to go through each file and post-process data
 for(i in 1:length(files)){
+#for(i in 2:length(files)){
   
   #read in csv raw hydraulics to be processed
   data <- read.csv(files[i])
@@ -38,12 +44,19 @@ for(i in 1:length(files)){
   vel.cols <- col.names[grep("Vel", col.names)]
   depth.cols2 <- col.names[grep("Depth", col.names)]
   #if vel < 0 change the depth to 0
-  data$Hydr..Depth..ft..LOB[data$Avg..Vel...ft.s..LOB < 0] <- 0
-  data$Hydr..Depth..ft..MC[data$Avg..Vel...ft.s..MC < 0] <- 0
-  data$Hydr..Depth..ft..ROB[data$Avg..Vel...ft.s..ROB < 0] <- 0
-  
+  #if more than one vel columns (LOB, MC, ROB), replace, else if only one (MC), only replace MC
+  if(length(vel.cols)> 1){
+    data$Hydr..Depth..ft..LOB[data$Avg..Vel...ft.s..LOB < 0] <- 0
+    data$Hydr..Depth..ft..MC[data$Avg..Vel...ft.s..MC < 0] <- 0
+    data$Hydr..Depth..ft..ROB[data$Avg..Vel...ft.s..ROB < 0] <- 0
+  }else{
+    data$Hydr..Depth..ft..MC[data$Avg..Vel...ft.s..MC < 0] <- 0
+  }
+
   #all other values, if negative, replace with 0 (velocity, shear, power)
-  variable.cols <- 5:17
+  #index of first variable col
+  ind.vars <- grep("Avg", names(data))
+  variable.cols <- ind.vars[1]:length(names(data))
   #if values < 0 change to 0
   sub2 <- replace(data[,variable.cols], data[,variable.cols]<0, 0)
   #save updated data back into data df
@@ -52,6 +65,5 @@ for(i in 1:length(files)){
   #write csv in outdir
   file.name <- paste0(out.dir, files.short[i])
   write.csv(data, file=file.name, row.names=FALSE)
-  
   
 }

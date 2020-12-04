d4714f354f271246d1d64445da421b1f4b6a768f
kristaniguchi <kristinetq@sccwrp.org>
2020-12-04 10:55:33 -0800

    use max depth to calc WSE and max depth for slices (LOB, MC, ROB)

--- hydraulics_post-processing.R
+++ hydraulics_post-processing.R
@@ -1,39 +1,95 @@
 #LAR Eflows Study
-#Post-process the hydraulic model output
+#Post-process the hydraulic model output. Overall:
   #change all depths < 0.1 to 0
   #if velocity is < 0, then depth should be zero
   #if [velocity, shear, or power] < 0, set value to 0
+#But first, find the max depth for each channel slice, then post process depths
+  #use the max channel depth to find water surface elevation (max depth + thalweg)
+  #use WSE to find slice depths
 
 
 
 #Hydraulics raw output directory
-#raw.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/hydraulics-results-v3-201007/"
 #scenarios hydraulics:
-raw.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/Scnearios_hydraulics_10132020/results-hydraulics/"
+raw.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/hydraulic-results-v4-201130/"
+#raw.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/Scnearios_hydraulics_10132020/results-hydraulics/"
 
+#list files
 files <- list.files(raw.dir, full.names = TRUE)
 files.short <- list.files(raw.dir)
+
+#node names from the file names, need to change based on file name structure
+#current conditions hydraulics
+#nodes <- gsub("hydraulic_ts_", "", files.short)
+# <- gsub(".csv", "", nodes)
+#scenario hydraulics node
+nodes <- gsub("_predictions.csv", "", files.short)
+
+#read in the channel geometry data used to calc max depth from WSE
+geom <- read.csv(file="C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/node-cross-sections/reporting-nodes-bank-elevations.csv")
+#replace name Main to MC to match hyd output name
+names(geom) <- gsub("Main", "MC", names(geom))
 
 #post-processed directory
 #out.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/post_processed_hydraulics_201008/"
 #scenario output dir
-out.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/Scnearios_hydraulics_10132020/results-hydraulics_postprocessed/"
+out.dir <- "C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/Results_Hydraulics/hydraulic-results-v4-201130/scenario_results-hydraulics_postprocessed/"
+
 
 #loop to go through each file and post-process data
 for(i in 1:length(files)){
-#for(i in 2:length(files)){
-  
+
   #read in csv raw hydraulics to be processed
   data <- read.csv(files[i])
-  
-  #find column names and do filters based on depth, velocity, and power
+  #replace typo in column name Dpth to Depth
+  names(data) <- gsub("Dpth", "Depth", names(data))
+  #find column names; will do filters based on depth, velocity, and power
   col.names <- names(data)
-  
-  ###depth filter####
   #find the col indices for depth
   depth.cols <- grep("Depth", col.names)
-  #add last col for max Dpth
-  depth.cols <- c(depth.cols, length(col.names))
+  #depth col names
+  depth.col.names <- col.names[depth.cols]
+  
+  #read in the geom data for node i
+  geom.i <- geom[geom$Node == nodes[i],]
+  #find col names
+  geom.i.colnames <- names(geom.i)
+  #update the LOB, Main, ROB depths from hyd.depth to max depth using WSE and min elevation
+  #find min elevation columns index
+  min.col.ind <- grep("Min", geom.i.colnames)
+  #determine if NA for LOB, ROB
+  ind.na <- which(is.na(geom.i[,min.col.ind]))
+  #if NA, remove from col ind list
+  if(length(ind.na) > 0){
+    #remove NA columns
+    min.col.ind <- min.col.ind[-ind.na]
+  }
+  #find min column names
+  min.col.names <- geom.i.colnames[min.col.ind]
+  #find thalweg (min) elevation
+  thalweg.elev <- min(geom.i[,min.col.ind])
+  #find WSE from the max channel depth and thalweg elevation
+  wse.data <- thalweg.elev + data$Max.Chl.Depth..ft.
+  
+  #### loop through the min elevations and recalc the max depth using WSE
+  for(j in 1:length(min.col.ind)){
+    #find channel position
+    slice <- gsub("Min.Elev..", "", min.col.names[j])
+    #find index of col that needs to be replaced
+    ind.replaced <- grep(slice, depth.col.names)
+    col.replace <- depth.col.names[ind.replaced]
+    #find max section depth
+    max.depth.j <- wse.data - geom.i[,min.col.ind[j]]
+    #if WSE is below the min slice depth (i.e. overbank slices), replace neg with zero
+    max.depth.j[max.depth.j < 0] <- 0
+    #replace hyd depth with max.depth
+    ind.replaced.overall <- grep(col.replace, col.names)
+    data[,ind.replaced.overall] <- max.depth.j
+    #change col name to Max..Depth..ft..LOB
+    names(data)[ind.replaced.overall] <- gsub("Hydr", "Max", col.replace)
+  }
+  
+  ###depth filter####
   #if depth < 0.1 change to 0
   sub <- replace(data[,depth.cols], data[,depth.cols]<0.1, 0)
   #save updated data back into data df
@@ -46,11 +102,11 @@ for(i in 1:length(files)){
   #if vel < 0 change the depth to 0
   #if more than one vel columns (LOB, MC, ROB), replace, else if only one (MC), only replace MC
   if(length(vel.cols)> 1){
-    data$Hydr..Depth..ft..LOB[data$Avg..Vel...ft.s..LOB < 0] <- 0
-    data$Hydr..Depth..ft..MC[data$Avg..Vel...ft.s..MC < 0] <- 0
-    data$Hydr..Depth..ft..ROB[data$Avg..Vel...ft.s..ROB < 0] <- 0
+    data$Max..Depth..ft..LOB[data$Avg..Vel...ft.s..LOB < 0] <- 0
+    data$Max..Depth..ft..MC[data$Avg..Vel...ft.s..MC < 0] <- 0
+    data$Max..Depth..ft..ROB[data$Avg..Vel...ft.s..ROB < 0] <- 0
   }else{
-    data$Hydr..Depth..ft..MC[data$Avg..Vel...ft.s..MC < 0] <- 0
+    data$Max..Depth..ft..MC[data$Avg..Vel...ft.s..MC < 0] <- 0
   }
 
   #all other values, if negative, replace with 0 (velocity, shear, power)

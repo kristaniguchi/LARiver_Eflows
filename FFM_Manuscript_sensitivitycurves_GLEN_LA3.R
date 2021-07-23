2bb7caad9a53893a5dbce70851887ecf53619ae1
kristaniguchi <kristinetq@sccwrp.org>
2021-07-23 15:15:18 -0700

    added stormdrain discharge reduction to curves

--- FFM_Manuscript_sensitivitycurves_GLEN_LA3.R
+++ FFM_Manuscript_sensitivitycurves_GLEN_LA3.R
@@ -27,6 +27,8 @@ unique.metrics <- unique(ffm.labels$metric)
 metrics.to.plot <- c("Wet_BFL_Mag_10", "DS_Mag_50") #, "Wet_BFL_Mag_50", "DS_Mag_90")
 #metrics.to.plot <- c("Wet_BFL_Mag_10", "Wet_BFL_Mag_50", "DS_Mag_50", "DS_Mag_90", "Peak_2")
 
+#Read in urban baseflow curves, Q predicted = Const + X.Coeff * (relevant WRP Q). for Q in cfs
+ubf.all <- read.csv("UBFRatingCurves_StormdrainReduction.csv") 
 
 #Read in FFM percentiles from scenarios WRP - all nodes
 #ffm.all <- read.csv("C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Data/RawData/FlowData_from_Jordy/Results-Scenarios/results_FFMs/FFM_percentiles_allnodes_scenarios_02172021.csv")
@@ -72,8 +74,11 @@ iterations <- read.csv("C:/Users/KristineT/SCCWRP/LA River Eflows Study - Genera
 #add in baseline iterations
 baseline.iterations <- read.csv("C:/Users/KristineT/SCCWRP/LA River Eflows Study - General/Reports/manuscripts/sensitivitycurves_FFMs/01_data/summary_seasonalWRP_node_baselineonly.csv") %>% 
   rename(dry_season=dry, wet_season=wet, spring=spr)
+
 #combine scenario wrp with baselin
 iterations <- add_row(iterations, baseline.iterations)
+#create copy of iterations cfs
+iterations.cfs <- iterations
 
 #change all the units of cfs to cms in ffm.labels df
 names(iterations) <-gsub("cfs", "cms", names(iterations))
@@ -106,6 +111,9 @@ ffm.all.join <- merge(ffm.all2, reporting.node.names, by= "ReportingNode") %>%
   mutate(ScenarioType2 = "WRP")
 
 
+#output dataframes for WRP discharge associated with lower limit intersections of sensitivity curves
+wrp.lowerlimits <- data.frame(matrix(NA, nrow=1, ncol=5))
+names(wrp.lowerlimits) <- c("limit1", "limit2", "limit1.2.names","node", "metric")
 
 #loop through the scenarios to plot percentiles scenarios curve
 #unique.nodes
@@ -117,6 +125,9 @@ for(i in 1:length(unique.nodes)){
   # #subset flow ranges to node i
   # ranges.sub <- ranges[ranges$Node == unique.nodes[i],]
   
+  #subset urban baseflow reduction equations
+  ubf.all.sub <- ubf.all[ubf.all$ReportingNode == unique.nodes[i],]
+
   #subset percentiles
   #subset WRP to node i
   ffm.sub <- ffm.all.join[ffm.all.join$ReportingNode == unique.nodes[i],]
@@ -129,26 +140,8 @@ for(i in 1:length(unique.nodes)){
   ffm.sub <- ffm.sub %>% 
     merge(iterations.node, by="Scenario")
   
-  #add in scenario WRPQ data for node i - urban scenarios
-  #find ind WRP50 and WRP100 scenarios and put i appropriate values
-  ind.WRP50 <- grep("WRP50", ffm.sub.urban$Scenario)
-  #WRP 100
-  ind.WRP100 <- grep("WRP100", ffm.sub.urban$Scenario)
   #find baseline dry, wet, spring for that node
   baselines <- iterations.node[iterations.node$Scenario == 0,]
-  
-  #Add in seasonal WRP Q, start with all 0 for WRP0 and will replace with values
-  ffm.sub.urban$dry_season <- 0
-  ffm.sub.urban$wet_season <- 0
-  ffm.sub.urban$spring <- 0
-  #replace all WRP50 with seasonal wrp/2
-  ffm.sub.urban$dry_season[ind.WRP50] <- baselines$dry_season/2
-  ffm.sub.urban$wet_season[ind.WRP50] <- baselines$wet_season/2
-  ffm.sub.urban$spring[ind.WRP50] <- baselines$spring/2
-  #replace all ind.WRP100 with seasonal wrp
-  ffm.sub.urban$dry_season[ind.WRP100] <- baselines$dry_season
-  ffm.sub.urban$wet_season[ind.WRP100] <- baselines$wet_season
-  ffm.sub.urban$spring[ind.WRP100] <- baselines$spring
   
   
   #loop to create sensitivity curves for each metric
@@ -157,8 +150,10 @@ for(i in 1:length(unique.nodes)){
     #subset percentiles
     #subset WRP to metric j
     ffm.sub.metric.j <-  ffm.sub[ffm.sub$metric == metrics.to.plot[j],]
-    #subset SUSTAIN scenarios to metric j
-    ffm.sub.metric.j.urban <-  ffm.sub.urban[ffm.sub.urban$metric == metrics.to.plot[j],]
+    #subset urban baseflow scenarios curves to metric j
+    ubf.all.sub.metric <-  ubf.all.sub[ubf.all.sub$Metric == metrics.to.plot[j],]
+    
+    
     
     ####################################
     ####POINT sensitivity Curves, plotting raw flow metric data for p10-p90
@@ -179,35 +174,7 @@ for(i in 1:length(unique.nodes)){
     data.plot <- pivot_longer(ffm.sub.metric.j, cols = c("p90", "p50", "p10"), names_to="Percentile", values_to="Value")
     data.plot$Percentile <- factor(data.plot$Percentile, levels = Percentile)
     
-    #pivot longer .urban to format correctly
-    data.plot.urban <- pivot_longer(ffm.sub.metric.j.urban, cols = c("p90", "p50", "p10"), names_to="Percentile", values_to="Value")
-    data.plot.urban$Percentile <- factor(data.plot.urban$Percentile, levels = Percentile)
-    #merge data with WRP data to plot in one graph with shape as ScenarioType
-    #make Scenario from WRP
-    data.plot2 <- data.plot %>% 
-      mutate(Scenario = as.character(data.plot$Scenario))
-    #merge sustain scenarios with WRP plot data, all SUSTAIN scenarios
-    data.plot.urban.merge <- data.plot.urban %>% 
-      bind_rows(data.plot2)
-    #merge with sustain scenario labels (already merged outside loop)
-    data.plot.urban.merge2 <- data.plot.urban.merge 
-    #replace NA for WRP scenarios with WRP
-    data.plot.urban.merge2$BMP_WRP[which(is.na(data.plot.urban.merge2$BMP_WRP))] <- "WRP"
-    #replace NA for WRP scenarios with WRP
-    data.plot.urban.merge2$UrbanBaseflow_name[which(is.na(data.plot.urban.merge2$UrbanBaseflow_name))] <- "WRP"
-    
-    
-    #subset to BMP WRP only - subset urban removal only
-    #subset to all scenarios except urban50 and urban0 (baseflow removal scenarios)
-    ind.urb50 <- grep("UBF50", data.plot.urban.merge2$Scenario)
-    ind.urb0 <- grep("UBF0", data.plot.urban.merge2$Scenario)
-    data.plot.urban.merge2.bmponly <- data.frame(data.plot.urban.merge2[-c(ind.urb50,ind.urb0),])
-    #subset to baseflow scenarios only
-    data.plot.urban.merge2.urbnonly <- data.plot.urban.merge2 %>% 
-      filter(BMP_WRP == "No BMPs") %>% 
-      filter(UrbanBaseflow_pctQ != "UBF100") %>% 
-      data.frame()
-    
+
     #if dry season metric, set x to dry season wrp
     dry.ind <- grep("Dry", metric.info$title_name)
     if(length(dry.ind) > 0){
@@ -216,10 +183,10 @@ for(i in 1:length(unique.nodes)){
         rename(seasonal.wrp.Q = dry_season)
       ffm.sub.metric.j <- ffm.sub.metric.j %>% 
         rename(seasonal.wrp.Q = dry_season)
-      ffm.sub.metric.j.urban <- ffm.sub.metric.j.urban %>% 
-        rename(seasonal.wrp.Q = dry_season)
-      data.plot.urban.merge2.urbnonly <- data.plot.urban.merge2.urbnonly %>% 
-        rename(seasonal.wrp.Q = dry_season)
+      #ffm.sub.metric.j.urban <- ffm.sub.metric.j.urban %>% 
+        #rename(seasonal.wrp.Q = dry_season)
+      #data.plot.urban.merge2.urbnonly <- data.plot.urban.merge2.urbnonly %>% 
+        #rename(seasonal.wrp.Q = dry_season)
       
       
       #x axis label for seasonal wrp
@@ -239,10 +206,10 @@ for(i in 1:length(unique.nodes)){
         rename(seasonal.wrp.Q = wet_season)
       ffm.sub.metric.j <- ffm.sub.metric.j %>% 
         rename(seasonal.wrp.Q = wet_season)
-      ffm.sub.metric.j.urban <- ffm.sub.metric.j.urban %>% 
-        rename(seasonal.wrp.Q = wet_season)
-      data.plot.urban.merge2.urbnonly <- data.plot.urban.merge2.urbnonly %>% 
-        rename(seasonal.wrp.Q = wet_season)
+      #ffm.sub.metric.j.urban <- ffm.sub.metric.j.urban %>% 
+        #rename(seasonal.wrp.Q = wet_season)
+      #data.plot.urban.merge2.urbnonly <- data.plot.urban.merge2.urbnonly %>% 
+        #rename(seasonal.wrp.Q = wet_season)
       
       
       #x axis label for seasonal wrp
@@ -270,13 +237,62 @@ for(i in 1:length(unique.nodes)){
     summary(fit.p10)
     #predict values 
     lower.bound <- predict(fit.p10, newdata = ffm.sub.metric.j)
+    #find the intersection of lower limits with the p50 curve
+    #fit p50
+    fit.p50 <- lm(p50 ~ seasonal.wrp.Q, data = ffm.sub.metric.j)
+    summary(fit.p50)
+    #predict values 
+    #predict values from 0 to 100 in case lower limit is outside of the baseline WRP discharge of 2 cms
+    predict.wrp <- data.frame(list(0:100))
+    names(predict.wrp) <- "seasonal.wrp.Q"
+    p50.predict <- predict(fit.p50, newdata = predict.wrp)
     
+    #find values for urban baseflow removal scenarios UBF0 and UBF50: Q predicted = Const + X.Coeff * (relevant WRP Q).
+    seasonal.wrp.Q.cfs <- ffm.sub.metric.j$seasonal.wrp.Q/0.028316846592
+    #50% reduction p10 curve
+    index.UBF50.p10 <- which(ubf.all.sub.metric$UBF == "UBF50" & ubf.all.sub.metric$Percentile == "p10")
+    UBF50.p10 <-  (ubf.all.sub.metric$Const[index.UBF50.p10] + (ubf.all.sub.metric$X.Coeff[index.UBF50.p10] * seasonal.wrp.Q.cfs)) * 0.028316846592 #convert back to cms
+    #50% reduction p50 curve
+    index.UBF50.p50 <- which(ubf.all.sub.metric$UBF == "UBF50" & ubf.all.sub.metric$Percentile == "p50")
+    UBF50.p50 <-  (ubf.all.sub.metric$Const[index.UBF50.p50] + (ubf.all.sub.metric$X.Coeff[index.UBF50.p50] * seasonal.wrp.Q.cfs)) * 0.028316846592 #convert back to cms
+    #50% reduction p90 curve
+    index.UBF50.p90 <- which(ubf.all.sub.metric$UBF == "UBF50" & ubf.all.sub.metric$Percentile == "p90")
+    UBF50.p90 <-  (ubf.all.sub.metric$Const[index.UBF50.p90] + (ubf.all.sub.metric$X.Coeff[index.UBF50.p90] * seasonal.wrp.Q.cfs)) * 0.028316846592 #convert back to cms
+    #100% reduction p10 curve
+    index.UBF0.p10 <- which(ubf.all.sub.metric$UBF == "UBF0" & ubf.all.sub.metric$Percentile == "p10")
+    UBF0.p10 <-  (ubf.all.sub.metric$Const[index.UBF0.p10] + (ubf.all.sub.metric$X.Coeff[index.UBF0.p10] * seasonal.wrp.Q.cfs)) * 0.028316846592 #convert back to cms
+    #100% reduction p50 curve
+    index.UBF0.p50 <- which(ubf.all.sub.metric$UBF == "UBF0" & ubf.all.sub.metric$Percentile == "p50")
+    UBF0.p50 <-  (ubf.all.sub.metric$Const[index.UBF0.p50] + (ubf.all.sub.metric$X.Coeff[index.UBF0.p50] * seasonal.wrp.Q.cfs)) * 0.028316846592 #convert back to cms
+    #100% reduction p90 curve
+    index.UBF0.p90 <- which(ubf.all.sub.metric$UBF == "UBF0" & ubf.all.sub.metric$Percentile == "p90")
+    UBF0.p90 <-  (ubf.all.sub.metric$Const[index.UBF0.p90] + (ubf.all.sub.metric$X.Coeff[index.UBF0.p90] * seasonal.wrp.Q.cfs)) * 0.028316846592 #convert back to cms
+    #make dataframe for the geom_lines p50 including no reductions
+    UBF50.data <- data.frame(cbind(ffm.sub.metric.j$seasonal.wrp.Q, ffm.sub.metric.j$p50, UBF50.p50, UBF0.p50))
+    names(UBF50.data)[1:2]<- c("seasonal.wrp.Q", "UBF100.p50")
+    #pivot longer to plot lines as diff colors and add to plot
+    UBF50.data.wider <- UBF50.data %>% 
+      pivot_longer(cols=c("UBF100.p50", "UBF50.p50", "UBF0.p50"), names_to = "Scenario", values_to = "Value") %>% 
+      data.frame()
+    #set levels and rename scenarios
+    UBF50.data.wider$Scenario <- factor(UBF50.data.wider$Scenario, levels = c("UBF100.p50", "UBF50.p50", "UBF0.p50"))
     
     #plot with stat smooth WRP curve
     p2 <- ggplot(ffm.sub.metric.j, aes(x=seasonal.wrp.Q, y = p50)) +
       #geom_point() +
-      geom_smooth(method = "lm", color="black") +
+      #geom_smooth(method = "lm", color="black") +
       geom_ribbon(aes(ymin=lower.bound, ymax=upper.bound), alpha=0.2) +
+      #add in ubf curves 100% reduction
+      geom_ribbon(aes(ymin=UBF0.p10, ymax=UBF0.p90), alpha=0.2, fill="red") +
+      #add in ubf curves 50% reduction
+      geom_ribbon(aes(ymin=UBF50.p10, ymax=UBF50.p90), alpha=0.5, fill="#fee090") +
+      #median curves for all scenarios
+      geom_smooth(UBF50.data.wider, method = "lm", mapping= aes(x=seasonal.wrp.Q, y=Value, color=Scenario, linetype=Scenario)) +
+      scale_colour_manual(values= c("black", "#fc8d59", "red"), labels= c("WRP Reduction", "50% Stormdrain Reduction", "100% Stormdrain Reduction"), name="") +
+      scale_linetype_manual(values= c("solid", "twodash", "dotted"), guide=FALSE) +
+      #guides(color=guide_legend(override.aes=list(fill=c("grey", "#fee090", "lightpink")))) +
+      guides(color=guide_legend(override.aes=list(fill=c("grey", "#fee090", "lightpink"), linetype = c("solid", "twodash", "dotted")))) +
+      #scale_linetype(guide = FALSE) +
       #labs(title = paste0("WRP Sensitivity Curve: ", unique.nodes[i]), subtitle=metric.info$title_name,
       #     color = "Legend") + 
       ylab(metric.title) +
@@ -286,7 +302,9 @@ for(i in 1:length(unique.nodes)){
       ylim(c(NA, y.max.WRP)) +
       theme_bw() + 
       theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+            panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+            legend.position = "bottom",
+            text = element_text(size=12))
     
     plot(p2)
 
@@ -306,13 +324,22 @@ for(i in 1:length(unique.nodes)){
       p.flowrange2 <- p2 + 
         geom_hline(yintercept=typha.low.limit, color = "blue", lty="dashed") +
         annotate(geom = "text", x = 0.4, y = typha.low.limit+.1, label = "Cattail Lower Limit") +
-        geom_hline(yintercept=willow.low.limit, color = "red", lty="dashed") +
-        annotate(geom = "text", x = 1, y = willow.low.limit+.1, label = "Willow Lower Limit") +
+        geom_hline(yintercept=willow.low.limit, color = "grey", lty="solid") +
+        annotate(geom = "text", x = 1.4, y = willow.low.limit+.1, label = "Willow Lower Limit") +
         #baseline WRP
         geom_vline(xintercept=baseline.metric, lty="dashed") +
         annotate(geom = "text", x = baseline.metric-0.05, y = 1.25, label = "Current WRP", angle = 90)
         
       print(p.flowrange2)
+      
+
+      # Approximate seasonal WRP when wet-season baseflow is lower limits for typha and willow (intersection of flow limit with curve p50)
+      predict.wrp.typha.willow <- approx(x = p50.predict, y = predict.wrp$seasonal.wrp.Q, xout = c(typha.low.limit, willow.low.limit))$y  
+      #add in node and metric
+      row <- c(predict.wrp.typha.willow, "Limit1: typha, Limit2: willow", unique.nodes[i], metric.title)
+      #save into output df of overall wrp.lowerlimits
+      wrp.lowerlimits <- rbind(wrp.lowerlimits, row)
+        
       
       #if dry-season plot rec limits
       if(length(dry.ind) > 0){
@@ -325,11 +352,19 @@ for(i in 1:length(unique.nodes)){
           annotate(geom = "text", x = 0.4, y = kayak.flow.limit+.1, label = "Kayaking Lower Limit")
         
         print(p.flowrange2)
+        
+        # Approximate seasonal WRP when wet-season baseflow is lower limits for kayaking
+        predict.wrp.kayak <- approx(x = p50.predict, y = predict.wrp$seasonal.wrp.Q, xout = kayak.flow.limit)$y  
+        #add in node and metric
+        row2 <- c(predict.wrp.kayak, NA,"Limit1: kayaking, Limit2: NA", unique.nodes[i], metric.title)
+        #save into output df of overall wrp.lowerlimits
+        wrp.lowerlimits <- rbind(wrp.lowerlimits, row2)
+        
       }
         
       #save
       file.name <- paste0(out.dir, unique.nodes[i], "_", metric.info$metric, "_WRP_Sensitivity_Curve_lowerlimits.jpg")
-      ggsave(p.flowrange2, filename=file.name, dpi=300, height=4, width=4)
+      ggsave(p.flowrange2, filename=file.name, dpi=300, height=6, width=6)
     }
     
     #overlay optimal flow range for GLEN only
@@ -338,21 +373,30 @@ for(i in 1:length(unique.nodes)){
       
       #steelhead migration and Cladophora limit
       steelhead.low.limit <- 62*0.028316846592
-      cladophora.low.limit <- 348*0.028316846592
+      cladophora.low.limit <- 1.75
       p.flowrange2 <- p2 + 
         geom_hline(yintercept=steelhead.low.limit, color = "blue", lty="dashed") +
-        annotate(geom = "text", x = 1.4, y = steelhead.low.limit+.1, label = "Steelhead Migration Lower Limit") +
+        annotate(geom = "text", x = 1.15, y = steelhead.low.limit+.1, label = "Steelhead Migration & Green Algae Lower Limit") +
         #baseline WRP
         geom_vline(xintercept=baseline.metric, lty="dashed") +
-        annotate(geom = "text", x = baseline.metric-0.05, y = 2.5, label = "Current WRP", angle = 90)
-        #geom_hline(yintercept=cladophora.low.limit, color = "red", lty="dashed") +
+        #annotate(geom = "text", x = baseline.metric-0.05, y = 2.5, label = "Current WRP", angle = 90) +
+        annotate(geom = "text", x = baseline.metric-0.04, y = 1, label = "Current WRP", angle = 90) +
+        geom_hline(yintercept=cladophora.low.limit, color = "red", lty="dashed") 
         #annotate(geom = "text", x = 1, y = cladophora.low.limit+.1, label = "Cladophora Lower Limit")
       
       print(p.flowrange2)
       
+      # Approximate seasonal WRP when wet-season baseflow is lower limits for steelhead and cladaphora (intersection of flow limit with curve p50)
+      predict.wrp.steelhead.clad <- approx(x = p50.predict, y = predict.wrp$seasonal.wrp.Q, xout = c(steelhead.low.limit, cladophora.low.limit))$y  
+      #add in node and metric
+      row <- c(predict.wrp.steelhead.clad, "Limit1: steelhead, Limit2: cladaphora", unique.nodes[i], metric.title)
+      #save into output df of overall wrp.lowerlimits
+      wrp.lowerlimits <- rbind(wrp.lowerlimits, row)
+      
+      
       #save
       file.name <- paste0(out.dir, unique.nodes[i], "_", metric.info$metric, "_WRP_Sensitivity_Curve_lowerlimits.jpg")
-      ggsave(p.flowrange2, filename=file.name, dpi=300, height=4, width=5)
+      ggsave(p.flowrange2, filename=file.name, dpi=300, height=6, width=6)
     }
   }
 }
